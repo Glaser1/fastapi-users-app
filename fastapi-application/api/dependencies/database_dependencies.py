a57c592a7d883c1aae9066953bfbdd7e121f4bd7
Aleksandr <sashapliskov@gmail.com>
2025-03-15 19:50:29 +0300

    rename get_access_tokens_db yield value

--- fastapi-application/api/dependencies/database_dependencies.py
+++ fastapi-application/api/dependencies/database_dependencies.py
@@ -10,9 +10,9 @@ if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
 
 
-async def get_access_tokens_db(session: Annotated["AsyncSession", Depends(db_helper.get_db)]):
-    yield AccessToken.get_access_db(session=session)
+async def get_access_tokens_db(session: Annotated["AsyncSession", Depends(db_helper.session_getter)]):
+    yield AccessToken.get_db(session=session)
 
 
-async def get_users_db(session: Annotated["AsyncSession", Depends(db_helper.get_db)]):
+async def get_users_db(session: Annotated["AsyncSession", Depends(db_helper.session_getter)]):
     yield User.get_db(session=session)
