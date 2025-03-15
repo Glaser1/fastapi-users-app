2d51470096ee323a509af267ee7012d72c40a5c8
Aleksandr <sashapliskov@gmail.com>
2025-03-15 19:51:26 +0300

    fix imports

--- fastapi-application/api/api_v1/fastapi_users.py
+++ fastapi-application/api/api_v1/fastapi_users.py
@@ -1,7 +1,7 @@
 from fastapi_users import FastAPIUsers
 
 from core.models import User
-from core.user_manager import get_user_manager
+from api.dependencies.user_manager import get_user_manager
 from api.dependencies.backend import authentication_backend
 
 
